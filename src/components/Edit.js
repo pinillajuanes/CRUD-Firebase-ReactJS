bb93161945cdf3d3e03c1c92521a311fb14961fb
mapache17 <mota.170903@gmail.com>
2023-11-29 16:50:46 -0500

    Actualiza función editar

--- src/components/Edit.js
+++ src/components/Edit.js
@@ -1,58 +1,46 @@
-import {useEffect, useState} from 'react'
-import { useNavigate, useParams } from 'react-router-dom'
-import { doc, updateDoc, getDoc } from 'firebase/firestore'
-import { db } from '../firebaseConfig/firebase'
-
+import React, { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { doc, updateDoc, getDoc } from 'firebase/firestore';
+import { db } from '../firebaseConfig/firebase';
 
 const Edit = () => {
-  const [autor, setAutor] = useState('')
-  const [titulo, setTitulo] = useState('')
-  const [genero, setGenero] = useState('')
-  const [publicacion, setPublicacion] = useState(0)
-  const navigate = useNavigate()
-  const {id} = useParams()
+  const [titulo, setTitulo] = useState('');
+  const [detalles, setDetalles] = useState('');
+  const navigate = useNavigate();
+  const { id } = useParams();
 
   const update = async (e) => {
-    e.preventDefault()
-    const libro = doc(db, "libros", id)
+    e.preventDefault();
+    const tarea = doc(db, "tareas", id);
     const data = {
-      autor: autor, titulo: titulo, genero: genero, publicacion: publicacion
+      titulo: titulo,
+      detalles: detalles,
+    };
+    await updateDoc(tarea, data);
+    navigate("/showall");
+  };
+
+  const getTareaByID = async (id) => {
+    const tarea = await getDoc(doc(db, "tareas", id));
+    if (tarea.exists()) {
+      setTitulo(tarea.data().titulo);
+      setDetalles(tarea.data().detalles);
+    } else {
+      console.log("No existe la tarea");
     }
-    await updateDoc(libro, data)
-    navigate("/")
-  }
-  
-  const getLibroByID = async (id) => {
-    const libro = await getDoc(doc(db, "libros", id))
-    if(libro.exists()) {
-      setAutor(libro.data().autor)
-      setTitulo(libro.data().titulo)
-      setGenero(libro.data().genero)
-      setPublicacion(libro.data().publicacion)
-    }else{
-      console.log("No existe el libro")
-    }
-  }
+  };
 
   useEffect(() => {
-    getLibroByID(id)
+    getTareaByID(id);
     // eslint-disable-next-line
-  }, [])
+  }, []);
+
   return (
     <div className='container'>
       <div className='row'>
         <div className='col'>
-          <h1 className='text-center'>Editar libro</h1>
+          <h1 className='text-center'>Editar tarea</h1>
           <form onSubmit={update}>
-            <div className='mb-3'>
-              <label className='form-label'>Autor</label>
-              <input
-                type='text'
-                className='form-control'
-                value={autor}
-                onChange={(e) => setAutor(e.target.value)}
-              />
-            </div>
             <div className='mb-3'>
               <label className='form-label'>Título</label>
               <input
@@ -63,21 +51,12 @@ const Edit = () => {
               />
             </div>
             <div className='mb-3'>
-              <label className='form-label'>Género</label>
+              <label className='form-label'>Detalles</label>
               <input
                 type='text'
                 className='form-control'
-                value={genero}
-                onChange={(e) => setGenero(e.target.value)}
-              />
-            </div>
-            <div className='mb-3'>
-              <label className='form-label'>Año</label>
-              <input
-                type='number'
-                className='form-control'
-                value={publicacion}
-                onChange={(e) => setPublicacion(e.target.value)}
+                value={detalles}
+                onChange={(e) => setDetalles(e.target.value)}
               />
             </div>
             <div className='d-grid gap-2'>
@@ -89,7 +68,7 @@ const Edit = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Edit
+export default Edit;
