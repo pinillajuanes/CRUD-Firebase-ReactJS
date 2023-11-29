750473ace55643e09364ffbac7abfc121f22be8e
Juanes Pinilla <juanpinpin@unisabana.edu.co>
2023-11-29 17:04:09 -0500

    Corrije función de editar tarea

--- src/components/Edit.js
+++ src/components/Edit.js
@@ -1,90 +1,71 @@
 import {useEffect, useState} from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import { doc, updateDoc, getDoc } from 'firebase/firestore'
+import { collection, addDoc, serverTimestamp } from 'firebase/firestore'
 import { db } from '../firebaseConfig/firebase'
+import { auth } from '../firebaseConfig/firebase';
 
 
 const Edit = () => {
-  const [autor, setAutor] = useState('')
   const [titulo, setTitulo] = useState('')
-  const [genero, setGenero] = useState('')
-  const [publicacion, setPublicacion] = useState(0)
+  const [detalles, setDetalles] = useState('')
   const navigate = useNavigate()
+  const tareasCollecion = collection(db, "tareas")
   const {id} = useParams()
 
   const update = async (e) => {
     e.preventDefault()
-    const libro = doc(db, "libros", id)
+    const tarea = doc(db, "tareas", id)
     const data = {
-      autor: autor, titulo: titulo, genero: genero, publicacion: publicacion
+      titulo: titulo, detalles: detalles, usuario: auth.currentUser.uid, fecha_creacion: serverTimestamp()
     }
-    await updateDoc(libro, data)
-    navigate("/")
+    await updateDoc(tarea, data)
+    navigate("/showall")
   }
   
-  const getLibroByID = async (id) => {
-    const libro = await getDoc(doc(db, "libros", id))
-    if(libro.exists()) {
-      setAutor(libro.data().autor)
-      setTitulo(libro.data().titulo)
-      setGenero(libro.data().genero)
-      setPublicacion(libro.data().publicacion)
+  const getTareaByID = async (id) => {
+    const tarea = await getDoc(doc(db, "tareas", id))
+    if(tarea.exists()) {
+      setTitulo(tarea.data().titulo)
+      setDetalles(tarea.data().detalles)
     }else{
-      console.log("No existe el libro")
+      console.log("No existe la tarea")
     }
   }
 
   useEffect(() => {
-    getLibroByID(id)
+    getTareaByID(id)
     // eslint-disable-next-line
   }, [])
   return (
     <div className='container'>
       <div className='row'>
         <div className='col'>
-          <h1 className='text-center'>Editar libro</h1>
+          <h1 className='text-center'>Editar tarea</h1>
           <form onSubmit={update}>
             <div className='mb-3'>
-              <label className='form-label'>Autor</label>
-              <input
-                type='text'
-                className='form-control'
-                value={autor}
-                onChange={(e) => setAutor(e.target.value)}
-              />
-            </div>
-            <div className='mb-3'>
-              <label className='form-label'>Título</label>
-              <input
-                type='text'
-                className='form-control'
-                value={titulo}
-                onChange={(e) => setTitulo(e.target.value)}
-              />
-            </div>
-            <div className='mb-3'>
-              <label className='form-label'>Género</label>
-              <input
-                type='text'
-                className='form-control'
-                value={genero}
-                onChange={(e) => setGenero(e.target.value)}
-              />
-            </div>
-            <div className='mb-3'>
-              <label className='form-label'>Año</label>
-              <input
-                type='number'
-                className='form-control'
-                value={publicacion}
-                onChange={(e) => setPublicacion(e.target.value)}
-              />
-            </div>
-            <div className='d-grid gap-2'>
-              <button type='submit' className='btn btn-primary'>
-                Actualizar
-              </button>
-            </div>
+                <label className='form-label'>Título</label>
+                <input
+                  type='text'
+                  className='form-control'
+                  value={titulo}
+                  onChange={(e) => setTitulo(e.target.value)}
+                />
+              </div>
+              <div className='mb-3'>
+                <label className='form-label'>Detalles</label>
+                <input
+                  type='text'
+                  className='form-control'
+                  value={detalles}
+                  onChange={(e) => setDetalles(e.target.value)}
+                />
+              </div>
+              <div className='d-grid gap-2'>
+                <button type='submit' className='btn btn-primary'>
+                  Guardar
+                </button>
+              </div>
           </form>
         </div>
       </div>
